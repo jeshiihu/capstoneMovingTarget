feb213905173d7f485caac8e9b3256a5dda897f9
jeshiihu <jeshiihu@gmail.com>
2018-02-13 20:16:18 +0000

    velocity calc

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import cv2
 import numpy as np
 import frame_convert2
 import math
+from decimal import *
+import datetime
+
 
 def init():
     hoopArray = []
@@ -16,7 +19,8 @@ def getDepthFrame():
     return freenect.sync_get_depth(0, freenect.DEPTH_MM)[0]
 
 def getFrames():
-    return (getVideoFrame(), getDepthFrame())
+    time = datetime.datetime.now()
+    return (getVideoFrame(), getDepthFrame(), time.microsecond)
 
 def trackObject(video, depth):
 
@@ -44,16 +48,12 @@ def trackObject(video, depth):
     return -1
 
 def translationTo3D(xFrame, yFrame, depth):
-    horizDegPerPixel = float(57 / 640)
-    vertDegPerPixel = float(43 / 480)
+    horizDegPerPixel = Decimal(57) / Decimal(640)
+    vertDegPerPixel = Decimal(43) / Decimal(480)
 
-    print((xFrame, yFrame))
-
-    xzAngle = float(-(320-xFrame) * horizDegPerPixel)
-    yzAngle = float((240-yFrame) * vertDegPerPixel)
+    xzAngle = (Decimal(-(320.0000-xFrame)) * horizDegPerPixel)
+    yzAngle = (Decimal(240.0000-yFrame) * vertDegPerPixel)
     
-    print((xzAngle, yzAngle))
-
     xDist = depth * math.sin(math.radians(xzAngle))
     yzHyp = depth * math.cos(math.radians(xzAngle))
 
@@ -62,7 +62,17 @@ def translationTo3D(xFrame, yFrame, depth):
 
     return (xDist, yDist, zDist)
 
-def get3DCoordinates():
+def calculateXVelocity(x1, x2, timedelta):
+    return ((x2-x1)/timedelta) * 1000
+
+def calculateYVelocity(y1, y2, timedelta):
+    return ((y2-y1)/timedelta) * 1000
+
+def calculateZVelocity(z1, z2, timedelta):
+    return ((z2-z1)/timedelta) * 1000
+
+
+def test3DCoordinates():
     frames = getFrames()
     cv2.imshow("Video", frames[0])
     object = trackObject(frames[0], frames[1])
@@ -70,16 +80,76 @@ def get3DCoordinates():
     if (object == -1):
         return
     coordinates = translationTo3D(object[0], object[1], object[2])
-##    print(coordinates[0], coordinates[1], coordinates[2])
+    print(coordinates[0], coordinates[1], coordinates[2])
+    
+def testVelocity():
+    while 1:
+        if (cv2.waitKey(5) & 0xFF) == 27:
+            return
+        firstFrames = getFrames()
+        cv2.imshow("Video", firstFrames[0])
+        object = trackObject(firstFrames[0], firstFrames[1])
+        if (object != -1):
+            firstCoordinates = translationTo3D(object[0], object[1], object[2])
+            break
+    while 1:
+        key = cv2.waitKey(5) & 0xFF
+        if key == 27:
+            return       
+        frames = getFrames()
+        cv2.imshow("Video", frames[0])
+        object = trackObject(frames[0], frames[1])
+        if (object != -1):
+            coordinates = translationTo3D(object[0], object[1], object[2])
+            xV = calculateXVelocity(firstCoordinates[0], coordinates[0], frames[2]-firstFrames[2])
+            yV = calculateYVelocity(firstCoordinates[1], coordinates[1], frames[2]-firstFrames[2])
+            zV = calculateZVelocity(firstCoordinates[2], coordinates[2], frames[2]-firstFrames[2])
+            firstFrames = frames
+            firstCoordinates = coordinates
+            print(yV)
+            
+def saveFrames():
+    print("saveFrames")
+    while 1:
+        if (cv2.waitKey(5) & 0xFF) == 27:
+            return
+        firstFrames = getFrames()
+        cv2.imshow("Video", firstFrames[0])
+        object = trackObject(firstFrames[0], firstFrames[1])
+        if (object != -1):
+            firstCoordinates = translationTo3D(object[0], object[1], object[2])
+            break
+    for i in range(0, 60):
+        key = cv2.waitKey(5) & 0xFF
+        if key == 27:
+            return       
+        frames = getFrames()
+        cv2.imshow("Video", frames[0])
+        object = trackObject(frames[0], frames[1])
+        if (object != -1):
+            coordinates = translationTo3D(object[0], object[1], object[2])
+            xV = calculateXVelocity(firstCoordinates[0], coordinates[0], frames[2]-firstFrames[2])
+            yV = calculateYVelocity(firstCoordinates[1], coordinates[1], frames[2]-firstFrames[2])
+            zV = calculateZVelocity(firstCoordinates[2], coordinates[2], frames[2]-firstFrames[2])
+            colors = {'yellow':(0, 0, 0)}
+            cv2.putText(frames[0], 'y1: ' + str(firstCoordinates[1]), (500,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['yellow'], 2)
+            cv2.putText(frames[0], 'y2: ' + str(coordinates[1]), (500,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['yellow'], 2)
+            cv2.putText(frames[0], 't1: ' + str(firstFrames[2]), (500,100), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['yellow'], 2)
+            cv2.putText(frames[0], 't2: ' + str(frames[2]), (500,140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['yellow'], 2)
+            cv2.putText(frames[0], 'yV: ' + str(yV), (0, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.6,colors['yellow'], 2)
+            cv2.imwrite("throw/throw" + str(i) + ".jpg", frames[0])
+            firstFrames = frames
+            firstCoordinates = coordinates
 
 
 if __name__ == "__main__":
     while 1:
-        get3DCoordinates()
-
-        if (cv2.waitKey(5) & 0xFF) == 27:
-                break
-            
+        test3DCoordinates()
+        key = cv2.waitKey(100) & 0xFF
+        if key == 27:
+            break
+        elif key == 32:
+            saveFrames()
     cv2.destroyAllWindows()
 
 
