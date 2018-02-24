4a1d5a4a489f51d49dc7ce0df6ae63eb93b3cbc8
jeshiihu <jeshiihu@gmail.com>
2018-02-24 22:50:29 +0000

    projectile motion guess

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ import frame_convert2
 
 lower = np.array([60, 0, 100])
 upper = np.array([80, 255, 255])
-colors = {'yellow':(0, 255, 255)}
+colors = {'yellow':(0, 255, 255), 'black':(0,0,0)}
+
+def init():
+    freenect.
 
 #   Returns video frame [Y][X][RGB]
 def getVideoFrame():
@@ -19,7 +22,7 @@ def getVideoFrame():
 def getDepthFrame():
     return freenect.sync_get_depth(0, freenect.DEPTH_MM)[0]
 
-#   Gets both frames and current time
+#   Gets both frames and current time in milliseconds
 def getFrames():
     time = datetime.datetime.now()
     video = getVideoFrame()
@@ -45,25 +48,34 @@ def trackObject(video, depth):
     cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     
     if len(cnts) <= 0:
-        return -1, -1, -1, -1
+        return video, -1, -1, -1
         
     c = max(cnts, key=cv2.contourArea)
     ((x, y), radius) = cv2.minEnclosingCircle(c)
     cv2.circle(video, (int(x), int(y)), 1, colors['yellow'], 3)
 
     if radius < 6:
-        return -1, -1, -1, -1
-            
+        return video, -1, -1, -1
+        
+    (xC, yC, zC) = pixelToCartesian(int(x), int(y), depth[int(y)][int(x)])
+        
     cv2.circle(video, (int(x), int(y)), int(radius), colors['yellow'], 2)
-    cv2.putText(video, 'x: ' + str(x), (500,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['yellow'], 2)
-    cv2.putText(video, 'y: ' + str(y), (500,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['yellow'], 2)
-    cv2.putText(video, 'radius: ' + str(radius), (500,100), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['yellow'], 2)
-    cv2.putText(video, 'depth: ' + str(depth[int(y)][int(x)]), (500,140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['yellow'], 2)
+##    cv2.putText(video, 'x: ' + str(x), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+##    cv2.putText(video, 'y: ' + str(y), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+##    cv2.putText(video, 'radius: ' + str(radius), (20,100), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+##    cv2.putText(video, 'depth: ' + str(depth[int(y)][int(x)]*0.039), (20,140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+
+    cv2.putText(video, 'x: ' + str(xC), (20,360), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+    cv2.putText(video, 'y: ' + str(yC), (20,400), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+    cv2.putText(video, 'z: ' + str(zC), (20, 440), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
 
     return video, int(x), int(y), depth[int(y)][int(x)]
 
 #   Translates pixel coordinates to cartesian coordinates
-def translationTo3D(xFrame, yFrame, depth):
+#   x = horizontal in frame
+#   y = vertical in frame
+#   z = distance from kinect plane
+def pixelToCartesian(xFrame, yFrame, depth):
     horizDegPerPixel = Decimal(57) / Decimal(640)
     vertDegPerPixel = Decimal(43) / Decimal(480)
 
@@ -78,14 +90,51 @@ def translationTo3D(xFrame, yFrame, depth):
 
     return xDist, yDist, zDist
 
-def calculateXVelocity(x1, x2, timedelta):
-    return ((x2-x1)/float(timedelta)) * 1000
+    
 
-def calculateYVelocity(y1, y2, timedelta):
-    return ((y2-y1)/float(timedelta)) * 1000
+def xVelocity(x1, x2, timedelta):
+    return ((x2-x1)/float(timedelta))
 
-def calculateZVelocity(z1, z2, timedelta):
-    return ((z2-z1)/float(timedelta)) * 1000
+def yVelocity(y1, y2, timedelta):
+    return ((y2-y1)/float(timedelta))
+
+def zVelocity(z1, z2, timedelta):
+    return ((z2-z1)/float(timedelta))
+
+def projectileMotion(x1, x2, y1, y2, timedelta, xGoal):
+    vX = xVelocity(x1, x2, timedelta)
+    vY = yVelocity(y1, y2, timedelta)
+    timeGoal = (xGoal-x1) / vX
+    yGoal = (vY * timeGoal) - (0.5 * 9.81 * (timeGoal ** 2)) + y1
+    return yGoal
+
+# Return frame with ball tracked, ball position, and frame time
+def getTrackedFrame():
+    while 1:
+        (video, depth, time) = getFrames()
+        (tracked, xFrame, yFrame, depth) = trackObject(video, depth)
+        if (xFrame != -1):
+            (x, y, z) = pixelToCartesian(xFrame, yFrame, depth)
+            return (tracked, x, y, z, time)
+
+
+def testPrediction():
+    for i in range(0, 3):
+        (frame, x, y, z, time) = getTrackedFrame()
+    (frame1, x1, y1, z1, time1) = getTrackedFrame()
+    cv2.imwrite("projectile/frame1.jpg", frame1)
+    (frame2, x2, y2, z2, time2) = getTrackedFrame()
+    timedelta = (time2-time1).total_seconds() * 1000
+    cv2.putText(frame2, 'Timedelta ' + str(timedelta), (450,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+    cv2.imwrite("projectile/frame2.jpg", frame2)
+    yGoal = projectileMotion(x1, x2, y1, y2, timedelta, 1000)
+    print(yGoal)
+    
+def mainRun():
+    (video, depth, time) = getFrames()
+    (tracked, xFrame, yFrame, depth) = trackObject(video, depth)
+    cv2.imshow("Video", video)
+    
 
 def testFilter():
     (video, depth, time) = getFrames()
@@ -175,11 +224,12 @@ def saveFrames():
 
 if __name__ == "__main__":
     while 1:
-        testFilter()
-        
+        mainRun()
         key = cv2.waitKey(5) & 0xFF
         if key == 27:
             break
+        elif key == 32:
+            testPrediction()
 
 
     cv2.destroyAllWindows()
