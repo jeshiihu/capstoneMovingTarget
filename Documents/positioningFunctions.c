b4482bb156c9cb68aadbf59f3bfd06d10f11d6f1
jeshiihu <jeshiihu@gmail.com>
2018-03-03 17:59:29 -0700

    working pwm dyamic

--- Documents/positioningFunctions.c
+++ Documents/positioningFunctions.c
@@ -1,4 +1,20 @@
+void bottomToCenter(){
+	/* 856 steps are needed to move  16.51 cm.
+	*  16.51cm/4cm per rev = 4.1275 revolutions.
+	*  4.1275 revs * 200 steps (full step) = 856 steps
+	*/
+	
+}
+
 INT16U calcSteps(int y){
+	/* 90 steps are required to move 1cm. This is calculated from the following:
+	* 1 Pulley has 20 teeth; Each tooth is seperated by 2mm. 
+	* Assuming 50% contact (timing belt to pulley) that is 10 teeth.
+	* 10 teeth * 2mm = 2cm in half a revolution.
+	* 1 revolution is therefore 4cm.
+	* 1 Revolution has 200 steps (full step) so 1cm is 1/4 of a revolution (90 steps)
+	*/ 
+	
     INT16U numStepsNeeded=0;
 
     //90 steps for 1 cm
@@ -11,10 +27,15 @@ INT16U calcSteps(int y){
 }
 
  enum Direction calcDirection(int y){
+	/* This function determines the correct direction given a y. 
+	* If y is negative, the board should be moving downwards; done
+	* by rotating the motor clockwise. If y is positive, board
+	* should move upwards and thus counterclockwise.
+	*/
     if (y<0){
-        return ccw;
+        return cw;
     }
     else{
-        return cw;
+        return ccw;
     }
 }
