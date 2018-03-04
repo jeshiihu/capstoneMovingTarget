97e419cdc621827f7a552bacf7ff6000a0308c94
jeshiihu <jeshiihu@gmail.com>
2018-03-04 00:28:08 +0000

    testing

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 import cv2
 from picamera import PiCamera
-from picamera.array import PiRGBArray
+from picamera.array import PiRGBAnalysis
 from picamera.streams import PiCameraCircularIO
 import time
 
@@ -11,40 +11,11 @@ videoSize = (640, 480)
 rotation = 180
 
 class FrameAnalysis(PiRGBAnalysis):
-    def analyze(frame):
+
+    def analyze(self, frame):
+        print "analyze"
         (tracked, x, y) = trackObject(frame)
         cv2.imshow('video', tracked)
-
-    def trackObject(video):
-        
-        hsv = cv2.cvtColor(video, cv2.COLOR_BGR2HSV)
-        
-        mask = cv2.inRange(hsv, lower, upper)
-        mask = cv2.erode(mask, None, iterations = 1)
-        mask = cv2.dilate(mask, None, iterations = 1)
-        
-        # TEST
-        cv2.imshow("Masked", mask)
-        
-        cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-        
-        if len(cnts) <= 0:
-            return video, -1, -1
-
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-
-        if radius < 6:
-            return video, -1, -1
-            
-        cv2.circle(video, (int(x), int(y)), 1, colors['yellow'], 3)
-        cv2.circle(video, (int(x), int(y)), int(radius), colors['yellow'], 2)
-        cv2.putText(video, 'x: ' + str(x), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
-        cv2.putText(video, 'y: ' + str(y), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
-        cv2.putText(video, 'radius: ' + str(radius), (20,100), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
-
-        return video, int(x), int(y)
-
 
 
 def init():
@@ -56,7 +27,37 @@ def init():
     camera.rotation = rotation
     camera.start_recording(analysis, format='bgr')
     time.sleep(2)
+   
+def trackObject(video):
     
+    hsv = cv2.cvtColor(video, cv2.COLOR_BGR2HSV)
+    
+    mask = cv2.inRange(hsv, lower, upper)
+    mask = cv2.erode(mask, None, iterations = 1)
+    mask = cv2.dilate(mask, None, iterations = 1)
+    
+    # TEST
+    cv2.imshow("Masked", mask)
+    
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    
+    if len(cnts) <= 0:
+        return video, -1, -1
+
+    c = max(cnts, key=cv2.contourArea)
+    ((x, y), radius) = cv2.minEnclosingCircle(c)
+
+    if radius < 6:
+        return video, -1, -1
+        
+    cv2.circle(video, (int(x), int(y)), 1, colors['yellow'], 3)
+    cv2.circle(video, (int(x), int(y)), int(radius), colors['yellow'], 2)
+    cv2.putText(video, 'x: ' + str(x), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+    cv2.putText(video, 'y: ' + str(y), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+    cv2.putText(video, 'radius: ' + str(radius), (20,100), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+
+    return video, int(x), int(y)   
+
 def getFrame():
     rawCapture.truncate(0)
     image = camera.capture(rawCapture, format='bgr', use_video_port=True)
