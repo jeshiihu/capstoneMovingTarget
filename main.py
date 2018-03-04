3c21d1432df6e0f39ac59eb2a3e95b26b75ab129
nikosomos <somos@ualberta.ca>
2018-03-03 17:05:34 -0700

    tracking

--- main.py
+++ main.py
@@ -23,13 +23,48 @@ def getFrame():
     image = camera.capture(rawCapture, format='bgr', use_video_port=True)
     return image.array
 
+def trackObject(video):
+    
+    hsv = cv2.cvtColor(video, cv2.COLOR_BGR2HSV)
+    
+    mask = cv2.inRange(hsv, lower, upper)
+    mask = cv2.erode(mask, None, iterations = 1)
+    mask = cv2.dilate(mask, None, iterations = 1)
+    
+    # TEST
+    cv2.imshow("Masked", mask)
+    
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    
+    if len(cnts) <= 0:
+        return video, -1, -1
+    
+    c = max(cnts, key=cv2.contourArea)
+    ((x, y), radius) = cv2.minEnclosingCircle(c)
 
+    if radius < 6:
+        return video, -1, -1
 
+    cv2.circle(video, (int(x), int(y)), 1, colors['yellow'], 3)
+    cv2.circle(video, (int(x), int(y)), int(radius), colors['yellow'], 2)
+    cv2.putText(video, 'x: ' + str(x), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+    cv2.putText(video, 'y: ' + str(y), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+    cv2.putText(video, 'radius: ' + str(radius), (20,100), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors['black'], 2)
+    
+    return video, int(x), int(y)
 
+def main():
+    init()
+    while 1:
+        frame = getFrame()
+        (tracked, x, y) = trackObject(frame)
+        cv2.imshow('video', tracked)
 
-    key = cv2.waitKey(5) & 0xFF
-    if key == 27:
-        camera.close()
-        break
+        key = cv2.waitKey(5) & 0xFF
+        if key == 27:
+            camera.close()
+            break
+
+main()
                  
             
