f0ab9a2ef5842fc79b80896a9b2074eeea72806e
jeshiihu <jeshiihu@gmail.com>
2018-03-24 11:33:51 -0600

    adding kelly's wonderful uart stuff ♥

---
+++ Uart/software/interfaceDriver/Debug/HWLIBS/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../HWLIBS/alt_16550_uart.c \
+../HWLIBS/alt_bridge_manager.c \
+../HWLIBS/alt_clock_manager.c \
+../HWLIBS/alt_fpga_manager.c 
+
+C_DEPS += \
+./HWLIBS/alt_16550_uart.d \
+./HWLIBS/alt_bridge_manager.d \
+./HWLIBS/alt_clock_manager.d \
+./HWLIBS/alt_fpga_manager.d 
+
+OBJS += \
+./HWLIBS/alt_16550_uart.o \
+./HWLIBS/alt_bridge_manager.o \
+./HWLIBS/alt_clock_manager.o \
+./HWLIBS/alt_fpga_manager.o 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+HWLIBS/%.o: ../HWLIBS/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM C Compiler 5'
+	armcc --cpu=Cortex-A9 --no_unaligned_access -Dsoc_cv_av -I"C:\Users\kgchin\Desktop\march10\uart\software\interfaceDriver\APP" -I"C:\Users\kgchin\Desktop\march10\uart\software\interfaceDriver\BSP" -I"C:\Users\kgchin\Desktop\march10\uart\software\interfaceDriver\BSP\OS" -I"C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include" -I"C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av" -I"C:\intelFPGA\17.0\embedded\ip\altera\hps\altera_hps\hwlib\include\soc_cv_av\socal" -I"C:\Users\kgchin\Desktop\march10\uart\software\interfaceDriver\HWLIBS" -I"C:\Users\kgchin\Desktop\march10\uart\software\interfaceDriver\uC-CPU\ARM-Cortex-A" -I"C:\Users\kgchin\Desktop\march10\uart\software\interfaceDriver\uC-CPU" -I"C:\Users\kgchin\Desktop\march10\uart\software\interfaceDriver\uC-LIBS" -I"C:\Users\kgchin\Desktop\march10\uart\software\interfaceDriver\uCOS-II\Ports" -I"C:\Users\kgchin\Desktop\march10\uart\software\interfaceDriver\uCOS-II\Source" --c99 --gnu -O0 -g --md --depend_format=unix_escaped --no_depend_system_headers --depend_dir="HWLIBS" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
