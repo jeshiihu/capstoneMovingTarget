66ef7b9b6767f99c1fded42578c5f5ceb4e24421
nikosomos <somos@ualberta.ca>
2018-04-02 19:09:19 -0600

    threading on left pi

--- LeftPiMain.py
+++ LeftPiMain.py
@@ -82,10 +82,39 @@ def trackObject(frame, time):
 
     return frame, int(x), int(y), time
 
-class LeftPiCameraAnalysis(PiRGBAnalysis):
-    
-    def __init__(self, camera):
-        super(LeftPiCameraAnalysis, self).__init__(camera)
+class ImageProcessor(threading.Thread):
+    def __init__(self, owner):
+        super(ImageProcessor, self).__init__()
+        self.stream = io.BytesIO()
+        self.event = threading.Event()
+        self.terminated = False
+        self.owner = owner
+        self.start()
+
+    def run(self):
+        # This method runs in a separate thread
+        while not self.terminated:
+            # Wait for an image to be written to the stream
+            if self.event.wait(1):
+                try:
+                    self.stream.seek(0)
+                    self.analyze(bytes_to_rgb(self.stream, owner.camera.resolution))
+                    self.owner.done = True
+                    # Read the image and do some processing on it
+                    #Image.open(self.stream)
+                    #...
+                    #...
+                    # Set done to True if you want the script to terminate
+                    # at some point
+                    #self.owner.done=True
+                finally:
+                    # Reset the stream and event
+                    self.stream.seek(0)
+                    self.stream.truncate()
+                    self.event.clear()
+                    # Return ourselves to the available pool
+                    with self.owner.lock:
+                        self.owner.pool.append(self)
 
     def analyze(self, frame):
         global trackedFrames
@@ -133,7 +162,51 @@ class LeftPiCameraAnalysis(PiRGBAnalysis):
 
         if checkProgram('bottomRight'):
             getBottomRight(frame)
-    
+
+class ProcessOutput(object):
+    def __init__(self, camera):
+        self.camera = camera
+        self.done = False
+        # Construct a pool of 4 image processors along with a lock
+        # to control access between threads
+        self.lock = threading.Lock()
+        self.pool = [ImageProcessor(self) for i in range(4)]
+        self.processor = None
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame; set the current processor going and grab
+            # a spare one
+            if self.processor:
+                self.processor.event.set()
+            with self.lock:
+                if self.pool:
+                    self.processor = self.pool.pop()
+                else:
+                    # No processor's available, we'll have to skip
+                    # this frame; you may want to print a warning
+                    # here to see whether you hit this case
+                    self.processor = None
+        if self.processor:
+            self.processor.stream.write(buf)
+
+    def flush(self):
+        # When told to flush (this indicates end of recording), shut
+        # down in an orderly fashion. First, add the current processor
+        # back to the pool
+        if self.processor:
+            with self.lock:
+                self.pool.append(self.processor)
+                self.processor = None
+        # Now, empty the pool, joining each thread as we go
+        while True:
+            with self.lock:
+                try:
+                    proc = self.pool.pop()
+                except IndexError:
+                    pass # pool is empty
+            proc.terminated = True
+            proc.join()    
     
 def startTCP():
     global conn
@@ -159,8 +232,8 @@ def startCamera():
     camera.annotate_background = Color('black')
     
     camera.start_preview(alpha=200)
-    analyzer = LeftPiCameraAnalysis(camera)
-    camera.start_recording(analyzer, 'bgr')
+    output = ProcessOutput(camera)
+    camera.start_recording(output, 'bgr')
     timeStart = datetime.datetime.now()
     
 def stopCamera():
