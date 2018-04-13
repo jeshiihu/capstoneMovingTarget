ed6084351024bd557a86426c420f70e2d9af7dcb
nikosomos <somos@ualberta.ca>
2018-04-13 14:48:00 -0600

    took out commented code

--- LeftPiMain.py
+++ LeftPiMain.py
@@ -3,7 +3,6 @@ import cv2
 from picamera import Color
 from picamera import PiCamera
 from picamera.array import PiRGBAnalysis
-import datetime
 import time
 import sys
 import json
@@ -32,7 +31,17 @@ lowerHSVBound = np.array([160, 140, 90])
 upperHSVBound = np.array([180, 255, 255])
 displayColors = {'yellow':(0, 255, 255), 'black':(0,0,0)}
 
-programStatus = { 'idle' : 0 , 'seek' : 1 , 'track' : 2 , 'receive' : 3, 'analyze': 4, 'test': 5, 'topLeft' : 6, 'bottomRight' : 7, 'initY': 8}
+programStatus = {
+                    'idle' : 0 ,
+                    'seek' : 1 ,
+                    'track' : 2 ,
+                    'receive' : 3,
+                    'analyze': 4,
+                    'test': 5,
+                    'topLeft' : 6,
+                    'bottomRight' : 7,
+                    'initY': 8
+                }
 
 HOST = '0.0.0.0'
 PORT = 5005
@@ -48,39 +57,21 @@ bottomRight = [[],[],[]]
 goalArea = {}
 throw = []
 
-scaleFactor = 1
-
-
-
 def initSerial():
     ser = serial.Serial(
         port ='/dev/ttyUSB0',
         baudrate=115200,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS
-)
+        bytesize=serial.EIGHTBITS)
     return ser
 
-def getMicroseconds():
-    time = datetime.datetime.now() - timeStart
-    time = (time.seconds*1000000) + time.microseconds
-    return time
-
-def getMilliseconds():
-    time = getMicroseconds()/1000
-    return time
-
 def trackObject(frame):
-    
-##    time = getMilliseconds()
-    
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lowerHSVBound, upperHSVBound)
     mask = cv2.erode(mask, None, iterations = 1)
     mask = cv2.dilate(mask, None, iterations = 1)
     cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    
     
     if len(cnts) <= 0:
         return frame, -1, -1
@@ -91,17 +82,6 @@ def trackObject(frame):
     if radius < 6:
         return frame, -1, -1
     
-    
-##    if checkProgram('track'):
-    cv2.circle(frame, (int(x), int(y)), 1, displayColors['yellow'], 3)
-    cv2.circle(frame, (int(x), int(y)), int(radius), displayColors['yellow'], 2)
-##    cv2.putText(frame, 'X: ' + str(x), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-##    cv2.putText(frame, 'Y: ' + str(y), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    cv2.putText(frame, 'Radius: ' + str(radius), (20,100), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-##        cv2.imwrite("throw/frame.jpg", frame)
-##        cv2.imwrite("throw/mask.jpg", mask)
-                    
-
     return frame, int(x), int(y)
 
 class LeftPiCameraAnalysis(PiRGBAnalysis):
@@ -126,9 +106,7 @@ class LeftPiCameraAnalysis(PiRGBAnalysis):
             return
             
         if checkProgram('track'):
-            time = camera.frame.timestamp
-##            if x == -1:
-##                return
+            time = camera.frame.timestamp/1000
             
             if curFrame < throwAwayFrames1:
                 curFrame += 1
@@ -149,7 +127,6 @@ class LeftPiCameraAnalysis(PiRGBAnalysis):
             
             trackFrames()
             setProgram('receive')
-##            setProgram('idle')
             return
 
         if checkProgram('topLeft'):
@@ -172,14 +149,11 @@ class LeftPiCameraAnalysis(PiRGBAnalysis):
                 curFrame += 1
             else:
                 getAverage('bottomRight', bottomRight)
-                print goalArea
                 setProgram('idle')
                 
         if checkProgram('initY'):
             yFactorAdjust(frame)
-                
-    
-    
+
 def startTCP(port):
     global conn
     tcpConnection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -192,25 +166,13 @@ def startTCP(port):
     
 def startCamera():
     global camera, timeStart, conn
-    conn.sendall("Picamera init")
     camera = PiCamera(resolution = videoSize, framerate = fps)
     time.sleep(2)
-    
-##    camera.shutter_speed = camera.exposure_speed
-##    camera.exposure_mode = 'off'
-##    g = camera.awb_gains
-##    camera.awb_mode = 'off'
-##    camera.awb_gains = g
-##    camera.vflip = True
+
     camera.annotate_background = Color('black')
-    
-    conn.sendall("Start Preview")
     camera.start_preview(alpha=200)
     analyzer = LeftPiCameraAnalysis(camera)
-    
-    conn.sendall("Start Recording")
     camera.start_recording(analyzer, 'bgr')
-    timeStart = datetime.datetime.now()
     
 def stopCamera():
     camera.stop_recording()
@@ -221,11 +183,9 @@ def closeTCP():
     conn.close()
     
 def init(port):
-    cv2.namedWindow("video")
-    
+    cv2.namedWindow("control")
     startTCP(port)
     startCamera()
-    print "Done Init"
     setProgram('idle')
     
 def shutdown():
@@ -246,8 +206,6 @@ def receiveFrames():
         tmp = conn.recv(BUFFER_SIZE)
         data = json.loads(tmp)
         normalizeRightFrames(data['frame1R'], data['frame2R'])
-##        trackedFrames["frame1R"] = data['frame1R']
-##        trackedFrames["frame2R"] = data['frame2R']
         setProgram('analyze')
 
 def normalizeRightFrames(frame1R, frame2R):
@@ -258,18 +216,10 @@ def normalizeRightFrames(frame1R, frame2R):
     frame1RY = frame1R[1] + yAdjust
     frame2RY = frame2R[1] + yAdjust
 
-    try:
-        slope = (float(frame2RY)-float(frame1RY)) / (float(frame2R[0])-float(frame1R[0]))
-    except ZeroDivisionError:
-        print "stupid 0's"
+    slope = (float(frame2RY)-float(frame1RY)) / (float(frame2R[0])-float(frame1R[0]))
 
-    if (slope != 0):
-        x1R = round(((frame1L[1]-frame1RY) / slope) + frame1R[0])
-        x2R = round(((frame2L[1]-frame2RY) / slope) + frame2R[0])
-    else:
-        xSpeed = (frame2R[0] - frame1R[0]) / (frame2R[2]-frame1R[2])
-        x1R = (xSpeed * (frame1R[2]-frame1L[2])) + frame1R[0]
-        x2R = (xSpeed * (frame2R[2]-frame2L[2])) + frame2R[0]
+    x1R = round(((frame1L[1]-frame1RY) / slope) + frame1R[0])
+    x2R = round(((frame2L[1]-frame2RY) / slope) + frame2R[0])
 
     frame1RFixed = (x1R-10, frame1L[1], frame1L[2])
     frame2RFixed = (x2R-10, frame2L[1], frame2L[2])
@@ -307,7 +257,6 @@ def yFactorAdjust(frame):
     rightFrame = data['frame1']
     global yAdjust
     yAdjust = leftFrame[1]-rightFrame[1]
-    print "yADjedus: ", yAdjust
     setProgram('idle')
     
 def getAverage(goalAreaLabel, xyzList):
@@ -323,28 +272,11 @@ def trackFrames():
     frame1 = trackedFrames['frame1Raw']
     frame2 = trackedFrames['frame2Raw']
 
-                
     tracked1, x1, y1 = trackObject(frame1[0])
     tracked2, x2, y2 = trackObject(frame2[0])
     
-    cv2.putText(tracked1, 'X: ' + str(x1), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    cv2.putText(tracked1, 'Y: ' + str(y1), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    cv2.putText(tracked1, 'Time: ' + str(frame1[1]), (20,140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-
-    cv2.putText(tracked2, 'X: ' + str(x2), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    cv2.putText(tracked2, 'Y: ' + str(y2), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    cv2.putText(tracked2, 'Time: ' + str(frame2[1]), (20,140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    
-    cv2.imwrite("throw/frame1.jpeg", tracked1)
-    cv2.imwrite("throw/frame2.jpeg", tracked2)
-    
     trackedFrames['frame1L'] = (x1, y1, frame1[1])
     trackedFrames['frame2L'] = (x2, y2, frame2[1])
-                
-
-def getMMCoor(leftFrame, rightFrame):
-    (x, y, z) = getCoordinates(leftFrame[0], leftFrame[1], rightFrame[0], rightFrame[1])
-    return x, y, z
 
 def analyzeFrames():
     if checkProgram('analyze'):
@@ -354,24 +286,21 @@ def analyzeFrames():
         (x,y,z) = getPrediction(pos1, pos2, trackedFrames["frame1L"][2], trackedFrames["frame2L"][2])
     
         setProgram('idle')
-        
-def sumOfSquares(a, b, c):
-    return math.sqrt((a**2)+(b**2)+(c**2))
+
+def getMMCoor(leftFrame, rightFrame):
+    (x, y, z) = getCoordinates(leftFrame[0], leftFrame[1], rightFrame[0], rightFrame[1])
+    return x, y, z
 
 def getCoordinates(leftI, leftJ, rightI, rightJ):
     (leftX, leftY) = fromCenter(leftI, leftJ)
     (rightX, rightY) = fromCenter(rightI, rightJ)
 
 
-##    z = ((cameraBaseLine * cameraFocalLength) / ((leftX - rightX) * cameraPixelSize)) * scaleFactor
-    z = 3800000
+    z = ((cameraBaseLine * cameraFocalLength) / ((leftX - rightX) * cameraPixelSize)) * scaleFactor
     x = (leftX * cameraPixelSize) * (z / cameraFocalLength) 
     y = (leftY * cameraPixelSize) * (z / cameraFocalLength)
     
     return x, y, z
-
-def mmToIn(x, y, z):
-    return x/25.4, y/25.4, z/25.4
 
 def getPrediction(pos1, pos2, time1, time2):
     topLeft = goalArea['topLeft']
@@ -386,9 +315,7 @@ def getPrediction(pos1, pos2, time1, time2):
     yDiff = y2-y1
     zDiff = z2-z1
     timeDiff = time2-time1
-    
 
-    
     xVelocity = xDiff/timeDiff
     yVelocity = yDiff/timeDiff
     zVelocity = zDiff/timeDiff
@@ -398,17 +325,12 @@ def getPrediction(pos1, pos2, time1, time2):
     yHit = ((yVelocity * timeHit) - (0.5*9.81*(timeHit**2)))*1000000 + y1
     zHit = (zVelocity * timeHit)*1000000 + z1
     
-    print yHit
-    
     xPoss, yPoss = getMotorPossibilities(topLeft[0], topLeft[1], bottomRight[0], bottomRight[1])
     nearestX, nearestY = getClosestMotorPoint(xGoal, yHit, xPoss, yPoss)
     nearestX, nearestY = changeToMotorDivisions(nearestX, nearestY)
-    print (0, nearestY)
     sendToFPGA(0,nearestY)
     
     return xGoal, yHit, zHit
-    
-    
 
 def getMotorPossibilities(x1, z1, x2, z2):
     xPossibilities = np.linspace(x1, x2, 33)
@@ -431,7 +353,6 @@ def sendToFPGA(nearestX, nearestZ):
     global ser
     if ser.isOpen():
         input = str(nearestX) + ',' + str(nearestZ)
-##        input = str(-4) + ',' + str(5)
         ser.write(input + '\n')
         out = ''
         time.sleep(0.5)
@@ -439,43 +360,6 @@ def sendToFPGA(nearestX, nearestZ):
 def fromCenter(i,j):
     return (i - videoCenter[0]), (videoCenter[1] - j)
 
-##def reverseDistortion(i, j):
-##    
-##    (iCenter, jCenter) = fromCenter(i,j)
-##    
-##    
-##    xDistorted = (iCenter) * cameraPixelSize
-##    yDistorted = (jCenter) * cameraPixelSize
-####    xDistorted = (i) * cameraPixelSize
-####    yDistorted = ((abs(j - 480)) * cameraPixelSize)
-##    rDSquared = (xDistorted * xDistorted) + (yDistorted * yDistorted)
-##    x = xDistorted * (1 + (cameraDistortion * rDSquared))
-##    y = yDistorted * (1 + (cameraDistortion * rDSquared))
-##    return x, y
-
-def velocity(pos1, pos2, timedelta):
-    return ((pos2-pos1)/float(timedelta))
-
-def getVelocities(pos1, pos2, time1, time2):
-    timedelta = time2-time1
-    xVel = velocity(pos1[0], pos2[0], timedelta)
-    yVel = velocity(pos1[1], pos2[1], timedelta)
-    zVel = velocity(pos1[2], pos2[2], timedelta)
-    return xVel, yVel, zVel
-    
-
-
-##def test():
-##    if checkProgram('test'):
-##        conn.sendall('test')
-##        while True:
-##            
-##            key = cv2.waitKey(5) & 0xFF
-##            if key == ord('e'):
-##                conn.sendall('idle')
-
-
-    
 def main():
     init(int(sys.argv[1]))
     global curFrame, topLeft, bottomRight, ser
@@ -484,7 +368,6 @@ def main():
         
         receiveFrames()
         analyzeFrames()
-##        test()
         
         key = cv2.waitKey(5) & 0xFF
         if key == 27:
@@ -501,8 +384,6 @@ def main():
             bottomRight = [[],[],[]]
             curFrame = 0
             setProgram('bottomRight')
-##        elif key == ord('t'):
-##            setProgram('test')
         elif key == ord('y'):
             setProgram('initY')
         
--- RightPiMain.py
+++ RightPiMain.py
@@ -3,12 +3,12 @@ import cv2
 from picamera import Color
 from picamera import PiCamera
 from picamera.array import PiRGBAnalysis
-import datetime
 import time as t
 import sys
 import json
 import socket
 import numpy as np
+
 # Camera Settings
 fps = 60
 videoSize = (640,480)
@@ -29,27 +29,13 @@ BUFFER_SIZE = 128
 throwAwayFrames = 3
 curFrame = 0
 
-def getMicroseconds():
-    time = datetime.datetime.now() - timeStart
-    time = (time.seconds*1000000) + time.microseconds
-    return time
-
-def getMilliseconds():
-    time = getMicroseconds()/1000
-    return time
-
 def trackObject(frame):
-   
-##    time = getMilliseconds()
     
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lowerHSVBound, upperHSVBound)
     mask = cv2.erode(mask, None, iterations = 1)
     mask = cv2.dilate(mask, None, iterations = 1)
     cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    
-##    cv2.imshow("mask", mask)
-    
     
     if len(cnts) <= 0:
         return frame, -1, -1
@@ -59,16 +45,6 @@ def trackObject(frame):
 
     if radius < 6:
         return frame, -1, -1
-    
-##    if checkProgram('track'):
-##        
-    cv2.circle(frame, (int(x), int(y)), 1, displayColors['yellow'], 3)
-    cv2.circle(frame, (int(x), int(y)), int(radius), displayColors['yellow'], 2)
-##    cv2.putText(frame, 'X: ' + str(x), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-##    cv2.putText(frame, 'Y: ' + str(y), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    cv2.putText(frame, 'Radius: ' + str(radius), (20,100), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['yellow'], 2)
-##    cv2.imwrite("throw/frame.jpg", frame)
-##    cv2.imwrite("throw/mask.jpg", mask)
 
     return frame, int(x), int(y)
 
@@ -84,24 +60,10 @@ class RightPiCameraAnalysis(PiRGBAnalysis):
         if checkProgram('idle'):
             return
 
-
-        # if checkProgram('test'):
-        # 	frame x, y, time = trackObject(frame)
-        # 	trackedFrames = {}
-        # 	if not trackedFrames.has_key("frame1R")
-        # 		return
-        # 	else
-        # 		jsonFrames = json.dumps(trackedFrames)
-       	# 		tcpConnection.sendall(jsonFrames)
-       	# 		return
-
-
         if checkProgram('track'):
             time = camera.frame.timestamp/1000
 
             if not trackedFrames.has_key("frame1Raw"):
-##                print "Frame1", camera.frame.index, time
-##                cv2.imwrite("throw/frame1.jpg", tracked)
                 trackedFrames["frame1Raw"] = (frame, time)
                 curFrame = 0
                 return
@@ -109,12 +71,8 @@ class RightPiCameraAnalysis(PiRGBAnalysis):
             if curFrame < throwAwayFrames:
                 curFrame += 1
                 return
-            
-            
+        
             if not trackedFrames.has_key("frame2Raw"):
-##                print "Frame2", camera.frame.index, camera.frame.timestamp/1000
-##                cv2.imwrite("throw/frame2.jpg", tracked)
-##                print(x, y, time)
                 trackedFrames["frame2Raw"] = (frame, time)
                 return
             
@@ -123,17 +81,11 @@ class RightPiCameraAnalysis(PiRGBAnalysis):
             return
         
         if checkProgram('track1'):
-            tracked, x, y = trackObject(frame)
             time = camera.frame.timestamp/1000
             
-##            if x == -1:
-##                return
-            cv2.putText(frame, 'X: ' + str(x), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-            cv2.putText(frame, 'Y: ' + str(y), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-            cv2.putText(frame, 'Time: ' + str(time), (20,140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
+            tracked, x, y = trackObject(frame)
             
             trackedFrames['frame1'] = (x, y, time)
-            t.sleep(0.1)
             setProgram('send')
             return
 
@@ -148,26 +100,13 @@ def startTCP(port):
 
 def startCamera():
     global camera, timeStart, tcpConnection
-    tcpConnection.recv(BUFFER_SIZE)
     camera = PiCamera(resolution = videoSize, framerate = fps)
     t.sleep(2)
     
-##    camera.shutter_speed = camera.exposure_speed
-##    camera.exposure_mode = 'off'
-##    g = camera.awb_gains
-##    camera.awb_mode = 'off'
-##    camera.awb_gains = g
-##    camera.vflip = False
     camera.annotate_background = Color('black')
-    tcpConnection.recv(BUFFER_SIZE)
-
     camera.start_preview(alpha=200)
     analyzer = RightPiCameraAnalysis(camera)
-    tcpConnection.recv(BUFFER_SIZE)
-
     camera.start_recording(analyzer, 'bgr')
-    timeStart = datetime.datetime.now()
-    
 
 def stopCamera():
     camera.stop_recording()
@@ -178,11 +117,9 @@ def closeTCP():
     tcpConnection.close()
 
 def init(port):
-    cv2.namedWindow("video")
-    
+    cv2.namedWindow("control")
     startTCP(port)
     startCamera()
-    print('finished init')
     setProgram('idle')
 
 def shutdown():
@@ -204,27 +141,11 @@ def sendFrames():
         setProgram('idle')
 
 def trackFrames():
-    
     frame1 = trackedFrames.pop('frame1Raw')
     frame2 = trackedFrames.pop('frame2Raw')
     
-##    print len(frame1[0][0])
-    
     tracked1, x1, y1 = trackObject(frame1[0])
     tracked2, x2, y2 = trackObject(frame2[0])
-
-##            if x == -1:
-##                return
-    cv2.putText(tracked1, 'X: ' + str(x1), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    cv2.putText(tracked1, 'Y: ' + str(y1), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    cv2.putText(tracked1, 'Time: ' + str(frame1[1]), (20,140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-
-    cv2.putText(tracked2, 'X: ' + str(x2), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    cv2.putText(tracked2, 'Y: ' + str(y2), (20,60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    cv2.putText(tracked2, 'Time: ' + str(frame2[1]), (20,140), cv2.FONT_HERSHEY_SIMPLEX, 0.6, displayColors['black'], 2)
-    
-    cv2.imwrite('throw/frame1.jpeg', tracked1)
-    cv2.imwrite('throw/frame2.jpeg', tracked2)
     
     trackedFrames['frame1R'] = (x1, y1, frame1[1])
     trackedFrames['frame2R'] = (x2, y2, frame2[1])
@@ -233,28 +154,14 @@ def listenForCommand():
     global trackedFrames
     if checkProgram('idle'):
         data = tcpConnection.recv(BUFFER_SIZE)
-##        if data == 'track':
-##            setProgram('track')
-##            trackedFrames = {}
         trackedFrames = {}
         setProgram(data)
-        # if data == 'test':
-        	# setProgram('test')
-        	# trackedFrames = {}
-    # if checkProgram('test'):
-    	# data = tcpConnection.recv(BUFFER_SIZE)
-        # if data == 'endTest':
-        	# setProgram('idle')
-            
 
 def main():
     init(int(sys.argv[1]))
-
     while True:
-
         listenForCommand()
         sendFrames()
-
 
         key = cv2.waitKey(5) & 0xFF
         if key == 27:
@@ -262,8 +169,4 @@ def main():
 
     shutdown()
 
-
-
 main()
-                 
-            
